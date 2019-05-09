7aadb0c205a4d3af0980526c0bbee4104f197821
Steve McKenzie <steve@stevemckenzie.ca>
2019-05-08 22:15:53 -0700

    fix creating react elements that use the new object format

--- modules/RouterContext.js
+++ modules/RouterContext.js
@@ -73,7 +73,7 @@ const RouterContext = createReactClass({
               props[prop] = element[prop]
         }
 
-        if (typeof components === 'object') {
+        if (typeof components === 'object' && !components.$$typeof) {
           const elements = {}
 
           for (const key in components) {
